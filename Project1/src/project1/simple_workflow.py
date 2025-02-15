fd357cc9315b0094e480fc677d34640e841e4bd2
Abdulqadir8 <aq98123@gmail.com>
2025-02-15 12:19:53 +0500

    stream method

--- Project1/src/project1/simple_workflow.py
+++ Project1/src/project1/simple_workflow.py
@@ -1,6 +1,36 @@
 from langgraph.func import entrypoint, task
 
 
+# @task
+# def task1():
+#     print("Task1")
+#     return "Task 1 Completed"
+
+# @task
+# def task2():
+#     print("Task2")
+#     return "Task 2 Completed"
+
+
+# @entrypoint()
+# def run_flow(input: str):
+#     print("Running Simple Workflow")
+
+#     task1_output = task1().result()
+#     task2_output = task2().result()
+
+#     return f"Workflow executed successfully with outputs: {task1_output} and {task2_output}."
+
+# #Run the workflow
+# def run():
+#     run_flow.invoke("Simple Input")
+
+
+
+
+#########################################################
+
+
 @task
 def task1():
     print("Task1")
@@ -14,13 +44,11 @@ def task2():
 
 @entrypoint()
 def run_flow(input: str):
-    print("Running Simple Workflow")
-
-    task1_output = task1().result()
+    task1_output = task1()
     task2_output = task2().result()
+    return f"Here is the result of ouputs: {task1_output} and {task2_output}."
 
-    return f"Workflow executed successfully with outputs: {task1_output} and {task2_output}."
 
-#Run the workflow
-def run():
-    run_flow.invoke("Simple Input")
+def run() -> str:
+    for event in run_flow.stream(input="Initiate Task"):
+        print(event)
